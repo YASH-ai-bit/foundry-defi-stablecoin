ea7ebb94f206e568aadcd88d031c000453cb4d55
Yash-ai-bit <gujjaryash7089@gmail.com>
2025-03-13 19:14:03 +0000

    fuzz tests and handler done

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+// narrow dow the process of running test 
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract Handler is Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+    uint256 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+
+    constructor(DSCEngine _engine, DecentralizedStableCoin _dsc) {
+        dsce = _engine;
+        dsc = _dsc;
+        address[] memory collateralTokens = dsce.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
+
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
+
+        dsce.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    ////////HELPER FUNCTIONS////////
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock){
+        if(collateralSeed % 2 == 0){
+            return weth;
+        }
+        return wbtc;
+    }
+}
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -70,3 +70,4 @@ contract DSCEngineTest is Test {
         assertEq(expectedWeth, actualWeth);
     }
 }
+
